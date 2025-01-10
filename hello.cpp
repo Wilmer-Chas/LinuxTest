57d53c6155938c49984ac7aaa561de2f3e1505ff
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-01-10 16:34:33 +0100

    second version

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Git can be fun?" << std::endl;
     return 0;
 }
 // it compiles within the linux enviroment "hello world" has been generated!
